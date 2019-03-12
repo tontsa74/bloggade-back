5b18ed2b9c454fac4ee7cfda273c4cbbf4f03489
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-12 09:31:12 +0200

    Initial commit

---
+++ src/main/java/fi/tuni/bloggadeback/BloggadeBackApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.bloggadeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggadeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggadeBackApplication.class, args);
+    }
+
+}
