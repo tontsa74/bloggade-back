b76bf4f22d9f978b9b8566cfa029a7ca2c9353d6
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-12 09:42:34 +0200

    Hello Bloggade

--- src/main/java/fi/tuni/bloggadeback/BloggadeBackApplication.java
+++ src/main/java/fi/tuni/bloggadeback/BloggadeBackApplication.java
@@ -1,13 +1,18 @@
 package fi.tuni.bloggadeback;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BloggadeBackApplication {
+public class BloggadeBackApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BloggadeBackApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello Bloggade");
+    }
 }
