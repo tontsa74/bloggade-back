a3763f73b1de901d38ce78ec8a8a4966fd2d13e7
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-12 13:16:44 +0200

    add ServerConfiguration addCorsMappings

---
+++ src/main/java/fi/tuni/bloggadeback/ServerConfiguration.java
@@ -0,0 +1,14 @@
+package fi.tuni.bloggadeback;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class ServerConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
