7ce8e266f797c626767c0b8680fd954fe4dc38d7
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-20 11:13:59 +0200

    add allowedOrigins , https://localhost:8080, http://localhost:8080

---
+++ src/main/resources/static/precache-manifest.de208103555f476c42a6f9ad5c61a0ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8a7524bf1b2bd131aecb",
+    "url": "./static/js/main.88a3d3aa.chunk.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "./static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "8a7524bf1b2bd131aecb",
+    "url": "./static/css/main.afe58c6d.chunk.css"
+  },
+  {
+    "revision": "8ae6daf32f5a1c71d9c127bba485fc38",
+    "url": "./index.html"
+  }
+];
