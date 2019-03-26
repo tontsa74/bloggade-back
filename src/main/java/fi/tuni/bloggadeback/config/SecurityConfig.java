29b4a1e9008ff11461dd8805754fa5b5f3d3b989
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-03-26 18:17:48 +0200

    add test login feature for demo user and admin

---
+++ src/main/java/fi/tuni/bloggadeback/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package fi.tuni.bloggadeback.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+
+/*    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                    .antMatchers("/api/private/accounts/*").hasRole(USER)
+                    .antMatchers("/api/private/admin/**").hasRole(ADMIN)
+                .and()
+                    .formLogin();
+    }*/
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/*").hasRole(USER)
+                .antMatchers("/api/private/admin/**").hasRole(ADMIN)
+                .and()
+                .formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("jaska").password("{noop}password").roles(USER)
+            .and()
+                .withUser("jussi").password("{noop}password").roles(USER, ADMIN);
+    }
+}
