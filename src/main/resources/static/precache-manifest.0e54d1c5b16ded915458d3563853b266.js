7e51f286a6099d6a4df69987ac43d464968a3e00
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-04-24 13:23:30 +0300

    implement frontend to static

---
+++ src/main/resources/static/precache-manifest.0e54d1c5b16ded915458d3563853b266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3b2e14421923dc6d4f62",
+    "url": "./static/js/main.a329b3b3.chunk.js"
+  },
+  {
+    "revision": "1f4a036f5a12e5747767",
+    "url": "./static/js/2.0bfcb4a9.chunk.js"
+  },
+  {
+    "revision": "3b2e14421923dc6d4f62",
+    "url": "./static/css/main.b3da46bc.chunk.css"
+  },
+  {
+    "revision": "3420fed9bfe4ae0c39e8572dceb7c63b",
+    "url": "./index.html"
+  }
+];
